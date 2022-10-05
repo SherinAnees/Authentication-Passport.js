0d26da38877c151bde0337da3ff49235848fca91
Sherin <Sherin>
2022-10-05 23:25:05 +0400

    Database added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 //login page
-router.get("/login", (req, res) => res.send("Login"));
+router.get("/login", (req, res) => res.send("login"));
 //register page
 router.get("/signup", (req, res) => res.send("Sign up"));
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 //Routes
-app.use("/", require("./routes/index"));
-app.use("/user", require("./routes/user"));
+app.use("/", require("./routes/user"));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
